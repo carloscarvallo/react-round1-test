072b4209ce7b43e4a749018341a962c347c1d40d
carloscarvallo <carloscarvallo@protonmail.com>
2018-09-04 10:37:27 -0400

    fixing according to eslint warnings,errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
@@ -11,7 +9,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+and save to reload.
         </p>
       </div>
     );
