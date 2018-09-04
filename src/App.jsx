47f1fd854fe26de5f660b3b5263e65b15c8fdea7
carloscarvallo <carloscarvallo@protonmail.com>
2018-09-04 10:51:39 -0400

    adding basic skel for main component using react-router

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import PostList from './components/PostList';
+import PostDetail from './components/PostDetail';
+import PostFeatured from './components/PostFeatured';
+
+const apiUrl = 'https://thewirecutter.com/wp-json/wp/v2/posts';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      selectedPost: null,
+    };
+  }
+
+  componentWillMount() {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(posts => this.setState({ posts, selectedPost: posts[0] }));
+  }
+
+  render() {
+    const { posts, selectedPost } = this.state;
+    return (
+      <Router>
+        <div className="container">
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <header className="blog-header py-3">
+                  <div className="row flex-nowrap justify-content-between align-items-center">
+                    <div className="col-12 text-center">
+                      <a className="blog-header-logo text-dark" href="/">Blog</a>
+                    </div>
+                  </div>
+                </header>
+                <PostFeatured article={posts[0]} />
+                <PostList
+                  onPostSelect={selectedPost => this.setState({ selectedPost })}
+                  posts={posts}
+                />
+              </React.Fragment>
+            )}
+          />
+          <Route path="/:id" component={() => <PostDetail article={selectedPost} />} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
